4d0558f4c9044efb5f156e46f20404af0748a4e7
rjgutmor-da <rjgutmor@gmail.com>
2026-08-04 14:00:57 -0400

    chore: saneamiento de credenciales y configuracion de cliente admin

--- scratch/delete_tarek_cxc_detalle.js
+++ scratch/delete_tarek_cxc_detalle.js
@@ -1,11 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
+import { obtenerClienteSupabaseAdmin } from '../scripts/config/cliente_supabase_admin.js';
 
-const supabaseUrl = 'https://uqrmmotcbnyazmadzfvd.supabase.co';
-const supabaseServiceKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVxcm1tb3RjYm55YXptYWR6ZnZkIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3MDEyODI2NCwiZXhwIjoyMDg1NzA0MjY0fQ.rcdIczkJN0dnfIL9XoCDgDq4V3Pczl8zrOPPWBC1BRE';
-
-const supabase = createClient(supabaseUrl, supabaseServiceKey, {
-    auth: { autoRefreshToken: false, persistSession: false }
-});
+const supabase = obtenerClienteSupabaseAdmin();
 
 async function run() {
   console.log("Deleting cxc_detalle for Tarek's anticipo...");
